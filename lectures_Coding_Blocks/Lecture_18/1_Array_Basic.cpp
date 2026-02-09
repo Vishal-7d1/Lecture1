e96351e0485110b20c872cf827c86c275b1cc9c6
Vishal-7d1 <vyadav276203@gmail.com>
2026-02-09 14:28:22 +0530

    Array

--- lectures_Coding_Blocks/Lecture_18/1_Array_Basic.cpp
+++ lectures_Coding_Blocks/Lecture_18/1_Array_Basic.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 int main(){
     int arr [5];
+    arr[0]=1;
+    arr[1]=1;
+    arr[2]=1;
+    arr[3]=1;
+    arr[4]=1;
     int arr_b[]={1,2,3,4,5};
     int n;
     cin>>n;
     int arr_c[n];
     int s=sizeof(arr)/sizeof(int);
+    for(int i=0;i<s;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<s;i++){
+        arr[i]+=i;
+    }
     for(int i=0;i<s;i++){
         cout<<arr[i]<<" ";
     }
---
+++ lectures_Coding_Blocks/Lecture_18/2_Grater_No.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[5];
+    arr[]=8;
+    arr[]=13;
+    arr[]=-21;
+    arr[]=49;
+    arr[]=30;
+    return 0;
+}
