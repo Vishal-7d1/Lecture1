22935356a01e483b082c4c3608d5865f8b7b576d
Vishal-7d1 <vyadav276203@gmail.com>
2026-02-09 14:10:36 +0530

    Array

--- lectures_Coding_Blocks/Lecture_18/1_Array_Basic.cpp
+++ lectures_Coding_Blocks/Lecture_18/1_Array_Basic.cpp
@@ -1,2 +1,14 @@
 #include<iostream>
 using namespace std;
+int main(){
+    int arr [5];
+    int arr_b[]={1,2,3,4,5};
+    int n;
+    cin>>n;
+    int arr_c[n];
+    int s=sizeof(arr)/sizeof(int);
+    for(int i=0;i<s;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
