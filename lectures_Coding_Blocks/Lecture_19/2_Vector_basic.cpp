342d56481c97ebbf30f6ba46e35eb0d466166a82
Vishal-7d1 <vyadav276203@gmail.com>
2026-02-10 16:04:53 +0530

    vector functions

--- lectures_Coding_Blocks/Lecture_19/2_Vector_basic.cpp
+++ lectures_Coding_Blocks/Lecture_19/2_Vector_basic.cpp
@@ -9,7 +9,23 @@ int main(){
     a.push_back(4);
     a.push_back(5);
     cout<<a.size()<<endl;
+
     cout<<a.capacity()<<endl;
+
+    a.pop_back();
+
+    cout<<a.size()<<endl;
+
+    cout<<a.capacity()<<endl;
+
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    cout<<a.front()<<endl;
+
+    cout<<a.back()<<endl;
+    a.insert(a.begin()+0,6);
     for(int i=0;i<a.size();i++){
         cout<<a[i]<<" ";
     }
