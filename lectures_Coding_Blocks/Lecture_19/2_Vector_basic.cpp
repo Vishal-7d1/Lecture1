a9898816d0a9b45dad5556d0c84e382682c41a3c
Vishal-7d1 <vyadav276203@gmail.com>
2026-02-11 23:51:26 +0530

    CPP

--- lectures_Coding_Blocks/Lecture_19/2_Vector_basic.cpp
+++ lectures_Coding_Blocks/Lecture_19/2_Vector_basic.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 int main(){
     vector<int> a;
@@ -29,6 +30,15 @@ int main(){
     for(int i=0;i<a.size();i++){
         cout<<a[i]<<" ";
     }
+    reverse(a.begin(),a.end());
+
+    sort(a.begin(),a.end());
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+
+
     return 0;
 
 }
