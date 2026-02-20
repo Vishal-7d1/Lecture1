0a7ca7d9c076d6813a1b5ee48e9e7f904eeb4505
Vishal-7d1 <vyadav276203@gmail.com>
2026-02-20 23:06:13 +0530

    note

---
+++ lectures_Coding_Blocks/Lecture_24/Sorting/3_Selection_Sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+        int min_index=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min_index]){
+                min_index=j;
+            }
+        }
+        swap(arr[i],arr[min_index]);
+    }
+     cout<<"Sorted array through selection sort "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+
+}
